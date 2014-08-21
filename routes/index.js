a927e0d5fbceb861fb30b91c3ed28126df9ceeed
Chris Allen <cehallen@gmail.com>
2014-08-21 16:56:03 -0500

    Started sqlite3

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,21 @@
 var express = require('express');
 var router = express.Router();
+var fs = require('fs');
+var file = "../JeopardyDatabase/jeopardy.db";
+var exists = fs.existsSync(file);
+var sqlite3 = require('sqlite3').verbose();
+var db = new sqlite3.Database(file);
+
 
 /* GET home page. */
 router.get('/', function(req, res) {
   // res.render('index', { title: 'Express' });
-  
+  var sqlQuery
+  var keyword = req.body.keyword;
+  var qAndA =
 });
+
+
+
 
 module.exports = router;
