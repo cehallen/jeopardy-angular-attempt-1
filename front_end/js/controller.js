5f58e269e20bf4f4215459693539119e4cf56dd5
Chris Allen <cehallen@gmail.com>
2014-08-21 22:15:11 -0500

    Test commit

--- front_end/js/controller.js
+++ front_end/js/controller.js
@@ -3,6 +3,6 @@ var jeopardyControllers = angular.module('jeopardyControllers', []);
 jeopardyControllers.controller('QuizMeCtrl', ['$scope', '$http',
   function($scope, $http) {
     $http.get('/quizme').success(function(data) {
-      $scope.qanda = data;
+      $scope.qAndA = data;
     });
   }]);
