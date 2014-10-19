ed9af43f3721294af79f2038ca5059673c289f7d
Chris Allen <cehallen@gmail.com>
2014-10-19 11:56:33 -0400

    moved DB in

--- server.js
+++ server.js
@@ -7,23 +7,12 @@ var bodyParser = require('body-parser');
 
 // db related
 var fs = require('fs');
-var file = "../JeopardyDatabase/jeopardy.db";
+var file = "JeopardyDatabase/jeopardy.db";
 var exists = fs.existsSync(file);
 var sqlite3 = require('sqlite3').verbose();
 var db = new sqlite3.Database(file);
-
-// async related
 var async = require('async');
-
-
-// var routes = require('./routes/index');
-// var users = require('./routes/users');
-
 var app = express();
-
-// view engine setup
-// app.set('views', path.join(__dirname, 'views'));
-// app.set('view engine', 'jade');
 
 app.use(favicon());
 app.use(logger('dev'));
@@ -32,23 +21,6 @@ app.use(bodyParser.urlencoded());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public'))); //this is super important
 
-// NOT NEEDED I THINK.  IF IT BREAKS PUT IT BACK BUT THIS WAS JUST AN EXP FROM SOME WEBSITE
-// app.use(function(req, res) {
-//   // Use res.sendfile, as it streams instead of reading the file into memory.
-//   res.sendfile('index.html');
-// });
-
-
-
-// GET to prompt for agegate.html before passing over to frontend app
-// app.get('/', function(req, res) {
-//   if (!req.cookies.over21) {
-//     res.sendfile('./agegate.html');
-//     return
-//   } else {
-//     res.sendfile('index.html');
-//   }
-// });
 
 // POST to respond with question/answer unit from user given keyword
 app.post('/api/quizme', function(req, res) {
@@ -69,23 +41,6 @@ app.post('/api/quizme', function(req, res) {
             qAndA = row;
             console.log(qAndA); // works √
             callback(null, qAndA);
-
-            // var sqlCategoryQuery = "SELECT * FROM category WHERE id = " + qAndA.category;
-            // var category = null;
-            // db.serialize(function() {
-            //   db.each(sqlCategoryQuery, function(err, row) {
-            //     if(err) {
-            //       console.log(err);
-            //     } else {
-            //       category = row;
-            //       qAndA.catName = category.name.toLowerCase(); // capitalize first letter w css
-            //       console.log(qAndA);
-            //     }
-            //   });
-            //   // console.log(qAndA);
-            //   // res.send(qAndA);
-            // });
-            // res.send(qAndA);  // *****
           }
         });
       });
@@ -101,33 +56,26 @@ app.post('/api/quizme', function(req, res) {
             console.log(err);
           } else {
             category = row;
-            qAndA.catName = category.name.toLowerCase();
+            string = category.name;
+            qAndA.catName = string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
             callback(null, qAndA);
           }
         });
       });
-
-      // callback(null, qAndA);
     }
   ], function(err, qAndA) {
     if (err) {
       res.send(err);
     } else {
-      // console.log(qAndA);
       res.send(qAndA);
     }
   });
 });
 
 // experiment inspired by http://stackoverflow.com/questions/23616485/expressjs-order-of-app-router-and-express-static
-app.use(function(req, res) {
-  // Use res.sendfile, as it streams instead of reading the file into memory.
-  res.sendfile('index.html');
-});
-
-// // from showtrakr, to shunt random urls to home.  not tested.  doesn't the .otherwise in app.js take care of this?
-// app.get('*', function(req, res) {
-//   res.redirect('/#' + req.originalUrl);
+// app.use(function(req, res) {
+//   // Use res.sendfile, as it streams instead of reading the file into memory.
+//   res.sendfile('index.html');
 // });
 
 /// catch 404 and forward to error handler
